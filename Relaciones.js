e43c901a510e43e302c68133ce719bf523493363
CaterinMB <127351464+CaterinMB@users.noreply.github.com>
2023-03-27 20:53:31 -0500

    Add files via upload

---
+++ Relaciones.js
@@ -0,0 +1,26 @@
+const { MongoClient, Db } = require("mongodb");
+const uri = "mongodb+srv://Pacho:Pacho12314@pach-os.bim9k0o.mongodb.net/test";
+const client = new MongoClient(uri);
+
+use("Pach_OS")
+db.Usuarios.aggregate([
+    {
+        $lookup: {
+            from: "Ventas",
+            localField: "id_Usuario",
+            foreignField: "id_Usuario",
+            as: "usuario_venta"
+        }
+    }
+])
+
+db.Productos.aggregate([
+    {
+        $lookup: {
+            from: "Ventas",
+            localField: "id_Producto",
+            foreignField: "id_Producto",
+            as: "producto_venta"
+        }
+    }
+])
